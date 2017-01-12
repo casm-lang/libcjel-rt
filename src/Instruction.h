4964473a7ee88d751a94d85a8f0d6d4ce7477191
Philipp Paulweber <p.paulweber@gmail.com>
2017-01-12 17:35:06 +0100

    Building and Implementation
    * added building files
    * added first implementation files

---
+++ src/Instruction.h
@@ -0,0 +1,58 @@
+//
+//  Copyright (c) 2017 CASM Organization
+//  All rights reserved.
+//
+//  Developed by: Philipp Paulweber
+//                https://github.com/casm-lang/libcsel-rt
+//
+//  This file is part of libcsel-rt.
+//
+//  libcsel-rt is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  libcsel-rt is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with libcsel-rt. If not, see <http://www.gnu.org/licenses/>.
+//
+
+/**
+   @brief    TODO
+
+   TODO
+*/
+
+#ifndef _LIB_CSELRT_INSTRUCTION_H_
+#define _LIB_CSELRT_INSTRUCTION_H_
+
+#include "CselIR.h"
+
+namespace libcsel_rt
+{
+    class Instruction : public CselRT
+    {
+      public:
+        static inline void compile( const libcsel_ir::AddInstruction& value )
+        {
+            value;
+            return;
+        }
+    };
+}
+
+#endif /* _LIB_CSELRT_INSTRUCTION_H_ */
+
+//
+//  Local variables:
+//  mode: c++
+//  indent-tabs-mode: nil
+//  c-basic-offset: 4
+//  tab-width: 4
+//  End:
+//  vim:noexpandtab:sw=4:ts=4:
+//
